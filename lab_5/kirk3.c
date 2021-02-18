c3630caf53375d2acb8e4d33cb129d521cafad4c
Shavin Croos <at015244@nxnode2.rdg.ac.uk>
2021-02-18 20:09:47 +0000

    Question 10 & 11

---
+++ lab_5/kirk3.c
@@ -0,0 +1,83 @@
+/*
+** kirk3.c -- writes to a message queue
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct my_msgbuf {
+    long mtype;
+    char mtext[200];
+};
+
+void sigint_handler(int sig)
+{
+  write(0, "\nSignal Terminated! Emergency transporter sent!\n", 50);
+}
+
+int main(void)
+{
+    void sigint_handler(int sig);
+    struct sigaction sa;
+
+    sa.sa_handler = sigint_handler;
+    sa.sa_flags = 0; // or SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGINT, &sa, NULL) == -1){
+      perror("sigaction");
+      exit(1);
+    }
+
+    struct my_msgbuf buf;
+    int msqid;
+    key_t key;
+
+    if ((key = ftok("kirk2.c", 'B')) == -1) {
+        perror("ftok");
+        exit(1);
+    }
+
+    if ((msqid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    printf("Enter lines of text, ^D to quit:\n");
+
+    buf.mtype = 1; /* we don't really care in this case */
+
+    while(fgets(buf.mtext, sizeof buf.mtext, stdin) != NULL) {
+      if(*buf.mtext >= 'a' && *buf.mtext <= 'z'){
+        int len = strlen(buf.mtext);
+
+        /* ditch newline at end, if it exists */
+        if (buf.mtext[len-1] == '\n') buf.mtext[len-1] = '\0';
+
+        if (msgsnd(msqid, &buf, len+1, 0) == -1) /* +1 for '\0' */
+            perror("msgsnd");
+    } else {
+      int len = strlen(buf.mtext);
+
+      /* ditch newline at end, if it exists */
+      if (buf.mtext[len-1] == '\n') buf.mtext[len-1] = '\0';
+
+      if (msgsnd(msqid, &buf, len+1, 0) == 0) /* +1 for '\0' */
+          perror("msgsnd");
+    }
+  }
+
+    if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        exit(1);
+    }
+
+    return 0;
+}
