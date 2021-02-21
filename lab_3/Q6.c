b1720d985de2cf665a85a374560d7213703e4bea
Shavin Croos <at015244@nxnode2.rdg.ac.uk>
2021-02-21 23:56:13 +0000

    Q6 updated

--- lab_3/Q6.c
+++ lab_3/Q6.c
@@ -6,8 +6,8 @@
 #include <unistd.h>
 
 int main(void) {
-  int pfds[2]; // Arrays denoting the pipe inputs and outputs
-  int pfds2[2];
+  int pfds[2]; // Size 2 arrays denoting the pipe inputs and outputs
+  int sndpfds[2];
 
   pipe(pfds);
 
@@ -17,7 +17,7 @@ int main(void) {
     exit(1);
 
     case 0:
-    pipe(pfds2); // If fork is in its child process, pipe 2nd array
+    pipe(sndpfds); // If fork is in its child process, pipe 2nd array
 
     switch (fork()) {
       case -1: // Error check
@@ -25,17 +25,17 @@ int main(void) {
       exit(2);
 
       case 0: // child of the next fork is cat, start switching standard input and output
-      dup2(pfds2[1], STDOUT_FILENO); // make stdout same as pfds2[1]
-      close(pfds2[0]); // close stdin
-      close(pfds2[1]); // close stdout
+      dup2(sndpfds[1], STDOUT_FILENO); // make stdout same as sndpfds[1]
+      close(sndpfds[0]); // close stdin
+      close(sndpfds[1]); // close stdout
       execlp("/usr/bin/cat", "/usr/bin/cat", "/etc/group", NULL); // place cat command in new out pipe
       exit(3);
 
       default: // check parent of current fork which is sort command
-      dup2(pfds2[0], STDIN_FILENO); // make stdin same as pfds2[0]
+      dup2(sndpfds[0], STDIN_FILENO); // make stdin same as sndpfds[0]
       dup2(pfds[1], STDOUT_FILENO); // make stdout same as pfds[1]
-      close(pfds2[0]); // close stdin
-      close(pfds2[1]); // close stdout
+      close(sndpfds[0]); // close stdin
+      close(sndpfds[1]); // close stdout
       execlp("/usr/bin/sort", "/usr/bin/sort", NULL); // place sort command in between out of pipe1 and in of pipe2
       exit(3);
     }
